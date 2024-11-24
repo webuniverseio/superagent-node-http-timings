67e964adbffcc3249a43036617969d6b9d97e663
Sergey Zarovskiy <other.book1352@fastmail.com>
2024-11-24 12:13:45 -0500

    Add tests for https://github.com/webuniverseio/superagent-node-http-timings/pull/20

--- index.test.js
+++ index.test.js
@@ -1,25 +1,41 @@
 const request = require('superagent');
 const logNetworkTime = require('./index');
+const https = require('https')
 jest.setTimeout(10000);
+
+const httpAgent = new https.Agent({keepAlive: true});
 describe('GIVEN plugin was used WHEN response arrives THEN callback should get timings information', () => {
   test('timings', () => {
     const assertions = [
       'verified that timings were populated'
     ];
     expect.assertions(assertions.length);
-    return expect(new Promise((res, rej) => {
-      request
-      .get(`https://google.com`)
-      .use(logNetworkTime((err, result) => {
-        if (err) {
-          return rej(err);
-        }
-        res(valuesToTypes(result));
-      }))
-      .then(x => x)
-      .catch(rej);
-    })).resolves.toMatchSnapshot();
+    return expect(makeRequest()).resolves.toMatchSnapshot();
   });
+
+  test('works with reusable keepAlive agent, we simply call the same request again with the same agent', () => {
+    const assertions = [
+      'verified that timings were populated'
+    ];
+    expect.assertions(assertions.length);
+    return expect(makeRequest()).resolves.toMatchSnapshot();
+  })
+
+  function makeRequest() {
+    return new Promise((res, rej) => {
+      request
+        .get(`https://google.com`)
+        .use(logNetworkTime((err, result) => {
+          if (err) {
+            return rej(err)
+          }
+          res(valuesToTypes(result))
+        }))
+        .agent(httpAgent)
+        .then(x => x)
+        .catch(rej)
+    })
+  }
 });
 
 function valuesToTypes(obj) {
